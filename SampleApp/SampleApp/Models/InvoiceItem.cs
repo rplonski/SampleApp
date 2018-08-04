45276d49358d6cbe329a36814d427951448eb5e6
rplo <rlo@tak.nie>
2018-08-04 22:04:01 +0200

    First models added

---
+++ SampleApp/SampleApp/Models/InvoiceItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp.Models
+{
+    public class InvoiceItem
+    {
+        public int VatRate { get; set; }
+        public double AmountNet { get; set; }
+        public double AmountGross { get; set; }
+    }
+}
